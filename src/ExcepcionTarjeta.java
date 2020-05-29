ee50eef7be6dd56e2162626213270c58bf94d273
faber <faber@DESKTOP-216U5VM>
2020-05-29 20:38:01 -0300

    Se modifica la Clase Tarjeta y se crea la Excepcion ExcepcionTarjeta

---
+++ src/ExcepcionTarjeta.java
@@ -0,0 +1,11 @@
+public class ExcepcionTarjeta extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7869083377575931277L;
+
+	public ExcepcionTarjeta (String mensaje) {
+		super(mensaje);
+	}
+}
--- src/Tarjeta.java
+++ src/Tarjeta.java
@@ -1,25 +1,40 @@
-
 public class Tarjeta {
+	
     public int numero;
     public int pin;
     
-    public Tarjeta(int numero, int pin) throws Exception{
-        if(contadorCaracteres(numero) == 8 && contadorCaracteres(pin)==4){
+    
+    
+    public Tarjeta(int numero, int pin) throws ExcepcionTarjeta {
+        if (contadorCaracteres(numero) == 8 && contadorCaracteres(pin) == 4) {
+        	
             this.numero=numero;
             this.pin = pin;
+            
+        } 
+        
+        else {
+            throw new ExcepcionTarjeta("La tarjeta debe tener 8 digitos y el pin 4.");
         }
-        else{
-            throw new Exception("La tarjeta debe tener 8 digitos y el pin 4");
-        }
+        
+        
     }
     
-    private int contadorCaracteres(int numero){
+    
+    
+    /**
+     * @POST Cuenta la cantidad de cifras de la variable numero
+     * 
+     * 
+     */
+    private int contadorCaracteres(int numero) {
         int cifras = 0;
         
-        while(numero !=0){
+        while (numero != 0){
             numero/=10;
             cifras++;            
         }        
+        
         return cifras;
         
     }
