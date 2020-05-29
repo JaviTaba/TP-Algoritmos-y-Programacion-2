3e35feafecfff412dea8b15df1f494c985ee3105
JaviTaba <javier.tabarovsky@gmail.com>
2020-05-29 18:26:20 -0300

    tarjeta completa

--- src/ArchivoDeTarjetas.java
+++ src/ArchivoDeTarjetas.java
@@ -1,17 +1,44 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 //Fabri Fabri 
 public class ArchivoDeTarjetas {
+	    
+    private HashMap<Long, Tarjeta> mapaTarjeta;
     
-    public void lector(){
+    public ArchivoDeTarjetas() {
+    	mapaTarjeta = new HashMap<Long, Tarjeta>();      
+    }
+
+	public void lectorDeTarjetas(){
         try {
-            FileReader fr = new FileReader("tarjetas.txt");
+        	  			
+        			
+        	FileReader fr = new FileReader("tarjetas.txt");
+            BufferedReader br = new BufferedReader(fr);
+            
+            String linea = br.readLine();
+            
+            while(linea != null) {
+            	String[] spliteado = linea.split(",");
+            	br.readLine();
+            	
+            long cuitDelUsuario = Long.parseLong(spliteado[2]);
+            
+            
+            }
+            
         } catch (FileNotFoundException ex) {
             System.out.println("No se encontro el archivo");
-        }
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
 }
