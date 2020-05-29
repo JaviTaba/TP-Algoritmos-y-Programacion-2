82545b97887004292bbbee7fb780b7c111d8e383
JaviTaba <javier.tabarovsky@gmail.com>
2020-05-29 18:24:25 -0300

    clase tarjeta terminada

--- src/Tarjeta.java
+++ src/Tarjeta.java
@@ -1,13 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author javi1
- */
 public class Tarjeta {
+    public int numero;
+    public int pin;
+    
+    public Tarjeta(int numero, int pin) throws Exception{
+        if(contadorCaracteres(numero) == 8 && contadorCaracteres(pin)==4){
+            this.numero=numero;
+            this.pin = pin;
+        }
+        else{
+            throw new Exception("La tarjeta debe tener 8 digitos y el pin 4");
+        }
+    }
+    
+    private int contadorCaracteres(int numero){
+        int cifras = 0;
+        
+        while(numero !=0){
+            numero/=10;
+            cifras++;            
+        }        
+        return cifras;
+        
+    }
     
 }
