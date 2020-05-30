83f4232591827a29b219b4bccae7584d222616bf
faber <faber@DESKTOP-216U5VM>
2020-05-30 10:51:45 -0300

    se corrige parcialmente la clase ArchivoDeCuentas

--- src/ArchivoDeCuentas.java
+++ src/ArchivoDeCuentas.java
@@ -1,46 +1,66 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
 public class ArchivoDeCuentas {
-
+	private HashMap<String, Cliente> clienteConAlias;
 	
-	private void lectorDeCuentas() throws IOException, ExcepcionCuenta {
-		
-		FileReader fr = new FileReader("cuentas.txt");
-		BufferedReader br = new BufferedReader(fr);
-		String linea = br.readLine();
-		ArchivoDeClientes usar2 = new ArchivoDeClientes();
-		HashMap<String, Cliente> clienteConAlias = usar2.getAliasCliente();
-		
-		
-		while(linea != null) {
-			String[] spliteado = linea.split(",");
+	public ArchivoDeCuentas() {
+		clienteConAlias = new HashMap<String, Cliente>();
+		lectorDeCuentas();
+	}
 	
-			switch(spliteado[0]) {
+	private void lectorDeCuentas() {
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("cuentas.txt"));
+			
+			try {
+			
+				String linea = br.readLine();
+				ArchivoDeClientes usar2 = new ArchivoDeClientes();
 				
-			case"01":
-				CajaAhorroPesos cuenta = new CajaAhorroPesos(spliteado[1], Integer.parseInt(spliteado[2]));
-				clienteConAlias.get(spliteado[1]).asociarPesos(cuenta);
-				break;
-				
-			case"02":
-				CuentaCorriente cuenta2 = new CuentaCorriente(spliteado[1],Integer.parseInt(spliteado[2]), Integer.parseInt(spliteado[3]));
-				clienteConAlias.get(spliteado[1]).asociarCuentaCorriente(cuenta2);
-				break;
-				
-			case"03":
-				CajaAhorroUSD cuenta3 = new CajaAhorroUSD(spliteado[1], Integer.parseInt(spliteado[2]));
-				clienteConAlias.get(spliteado[1]).asociarUSD(cuenta3);
-				break;
+		
+		
+				while(linea != null) {
+					String[] spliteado = linea.split(",");
+			
+					switch(spliteado[0]) {
+						
+					case"01":
+						CajaAhorroPesos cuenta = new CajaAhorroPesos(spliteado[1], Double.parseDouble(spliteado[2]));
+						clienteConAlias.get(spliteado[1]).asociarPesos(cuenta);
+						break;
+						
+					case"02":
+						CuentaCorriente cuenta2 = new CuentaCorriente(spliteado[1],Double.parseDouble(spliteado[2]), Double.parseDouble(spliteado[3]));
+						clienteConAlias.get(spliteado[1]).asociarCuentaCorriente(cuenta2);
+						break;
+						
+					case"03":
+						CajaAhorroUSD cuenta3 = new CajaAhorroUSD(spliteado[1], Double.parseDouble(spliteado[2]));
+						clienteConAlias.get(spliteado[1]).asociarUSD(cuenta3);
+						break;
+				    } 
+					
+					linea = br.readLine();
+				}
+			} finally {
+				br.close();
 			}
-			
-		
-			
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		} catch (Exception ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
 		}
 		
-		br.close();
+		
 		
 		
 		
