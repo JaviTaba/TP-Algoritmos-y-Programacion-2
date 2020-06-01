90b0cd887b9e3d31e86053bf987d41600647a74f
faber <faber@DESKTOP-216U5VM>
2020-06-01 03:38:18 -0300

    Se corrige clase Ticket y Cuenta

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,6 +1,5 @@
-import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Ticket {
@@ -19,11 +18,60 @@ public class Ticket {
 	public String toString() {
 		return dateFormat.format(date);
 	}
-
+	
+	public String extraer(Cuenta cuenta, double monto){
+		return "-------------------------------------------------------"
+				+ "\n Has realizado una EXTRACCIÓN"
+				+ "\n FECHA: "+toString()
+				+ "\n IMPORTE: $"+monto
+				+ "\n SALDO ACTUAL: $"+cuenta.getSaldo()
+				+ "\n-------------------------------------------------------";
+	}
+	
+	public String transferir(Cuenta cuenta, double monto) {
+		return "-------------------------------------------------------"
+				+ "\n Has realizado una TRANSFERENCIA"
+				+ "\n FECHA: "+toString()
+				+ "\n IMPORTE: $"+monto
+				+ "\n SALDO ACTUAL: $"+cuenta.getSaldo()
+				+ "\n-------------------------------------------------------";
+	}
+	
+	public String depositar(Cuenta cuenta, double monto) {
+		return "-------------------------------------------------------"
+				+ "\n Has realizado un DEPOSITO"
+				+ "\n FECHA: "+toString()
+				+ "\n IMPORTE: $"+monto
+				+ "\n SALDO ACTUAL: $"+cuenta.getSaldo()
+				+ "\n-------------------------------------------------------";
+	} 
+	
+	public String comprarUSD(Cuenta cuenta, double monto) {
+		DecimalFormat df = new DecimalFormat("#.00");
 		
+		return "-------------------------------------------------------"
+				+ "\n Has realizado una COMPRA DE DOLARES"
+				+ "\n FECHA: "+toString()
+				+ "\n IMPORTE: $"+df.format(monto/cuenta.dolar)
+				+ "\n SALDO ACTUAL: $"+cuenta.getSaldo()
+				+ "\n-------------------------------------------------------";
+	} 
 	
+	public String saldo(Cuenta cuenta) {
+		return "-------------------------------------------------------"
+				+ "\n Has realizado una consulta de SALDO"
+				+ "\n FECHA: "+toString()
+				+ "\n SALDO ACTUAL: $"+cuenta.getSaldo()
+				+ "\n-------------------------------------------------------";
+	} 
 	
-	
+	public String alias(Cuenta cuenta) {
+		return "-------------------------------------------------------"
+				+ "\n Has realizado una consulta de ALIAS"
+				+ "\n FECHA: "+toString()
+				+ "\n ALIAS: "+cuenta.getAlias()
+				+ "\n-------------------------------------------------------";
+	} 
 	
 	
 	
