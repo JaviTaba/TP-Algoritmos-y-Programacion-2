2c342ede7bd5c8d79c56d5ddec9820a6c0a551a5
faber <faber@DESKTOP-216U5VM>
2020-06-12 23:01:07 -0300

    .

--- src/ModificarTextos.java
+++ src/ModificarTextos.java
@@ -3,10 +3,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.Random;
 
 public class ModificarTextos {
 	
